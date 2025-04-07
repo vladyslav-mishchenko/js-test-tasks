82c28d8603271810440fa25b15faefaf9b41190d
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-04-07 16:48:37 +0300

    Add enter text to target work block

--- src/js/text-builder.js
+++ src/js/text-builder.js
@@ -1,1 +1,13 @@
-console.log('hello js!');
+import { el, els, ev } from "./dom.js";
+
+const sourcetext = el('#sourcetext');
+const targettext = el('#targettext');
+const enterbtn = el('#enterbtn');
+
+// element:event:function
+ev(enterbtn, 'click', enterText);
+
+function enterText(){
+    targettext.innerHTML = sourcetext.value;
+    sourcetext.value = "";
+}
